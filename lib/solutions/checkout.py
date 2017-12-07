2caf431fd7bf852604fe75d4704dabc1d727078a
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-07 09:26:23 +0000

    Thu Dec 07 09:26:23 GMT 2017

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,10 +1,12 @@
 
+
 PRICE_UNIT = {
     'A': 50,
     'B': 30,
     'C': 20,
     'D': 10
 }
+
 
 SPECIAL_OFFERS = {
     'A': {
@@ -23,7 +25,20 @@ def deserialize(skus):
 
 
 def calculate_price(item_type, number):
+    # First check if there is a special offer.
+    if item_type not in SPECIAL_OFFERS.keys():
+        return PRICE_UNIT[item_type] * number
 
+    # Now we know that's a special offer, we should know how many items are affected by the promotions
+    offer_information = SPECIAL_OFFERS[item_type]
+
+    # Check the number of special offers per product
+    number_of_offers = number / offer_information['number']
+
+    # Check the number of products not usable by the special offer
+    left_over_products = number % offer_information['number']
+
+    return number_of_offers * offer_information['price'] + left_over_products * PRICE_UNIT[item_type]
 
 
 # noinspection PyUnusedLocal
@@ -38,12 +53,11 @@ def checkout(skus):
     # Count the number of items per product
     items_counter = {i:product_list.count(i) for i in product_list}
 
-    # For each product, count the total price
     total_price = 0
 
     for item, number in items_counter:
-        item_total_price = calculate_price(item, number)
+        # For each product, count the total price
+        # And add it to the total of each product
+        total_price += calculate_price(item, number)
 
-    # Sum the total of each product
-
-    raise NotImplementedError()
+    return total_price
