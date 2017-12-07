8b6df9ce7ccfd386986751f577dbcb0feb11f836
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-07 09:31:03 +0000

    Thu Dec 07 09:31:03 GMT 2017

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -50,6 +50,9 @@ def checkout(skus):
     # If I'm wrong with the assumed format, this will be the only step to be changed.
     product_list = deserialize(skus)
 
+    if not product_list:
+        return 0
+
     # Count the number of items per product
     items_counter = {i:product_list.count(i) for i in product_list}
 
