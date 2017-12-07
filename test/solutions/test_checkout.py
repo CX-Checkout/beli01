d1bd31fbd171064b295c31cbd265e6fdfefc51af
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-07 10:37:07 +0000

    Thu Dec 07 10:37:07 GMT 2017

--- test/solutions/test_checkout.py
+++ test/solutions/test_checkout.py
@@ -8,6 +8,24 @@ class TestCheckout(unittest.TestCase):
     def test_checkout_special_offer_2_for_A_products(self):
         self.assertEqual(checkout('AAAAA'), 200)
 
+    def test_checkout_special_offer_1_and_2_for_A_products(self):
+        self.assertEqual(checkout('AAAAAAAA'), 330)
+
+    def test_checkout_special_offer_1_and_2_and_remaining_for_A_products(self):
+        self.assertEqual(checkout('AAAAAAAAA'), 380)
+
+    def test_checkout_special_offer_for_B_products(self):
+        self.assertEqual(checkout('BB'), 45)
+
+    def test_checkout_special_offer_and_remaining_for_B_products(self):
+        self.assertEqual(checkout('BBB'), 75)
+
+    def test_checkout_inter_product_for_E_and_special_offer_for_B_products(self):
+        self.assertEqual(checkout('BEE'), 80)
+
+    def test_checkout_inter_product_for_E_and_special_offer_and_remaining_for_B_products(self):
+        self.assertEqual(checkout('BBEE'), 110)
+
 
 if __name__ == '__main__':
     unittest.main()
