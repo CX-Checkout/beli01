0337143adce665c945d48848b48501ee1302248e
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-07 09:17:44 +0000

    Thu Dec 07 09:17:44 GMT 2017

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,3 +1,21 @@
+def deserialize(skus):
+    return skus.replace(' ', '').split(',')
+
+
 # noinspection PyUnusedLocal
 def checkout(skus):
+    # Let's assume that the skus format is "A, B, C, A, D"
+    # Let's also assume that a special offer can be applied multiple times, i.e. "3A for 130" makes 6A cost 260
+
+    # First extract the skus in a more proper structure to deal with
+    # If I'm wrong with the assumed format, this will be the only step to be changed.
+    product_list = deserialize(skus)
+
+    # Count the number of items per product
+    items_counter = {i:product_list.count(i) for i in product_list}
+
+    # For each product, count the total price
+
+    # Sum the total of each product
+
     raise NotImplementedError()
